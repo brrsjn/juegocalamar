182d8ee825089fbd60a36b8bc840b6a5c4bb4573
jibarros <jibarros@alumnos.inf.utfsm.cl>
2021-11-05 01:07:02 -0300

    se añade el pozo con un ejemplo funcional, interaccion Lider + Pozo

--- service/lider_service.go
+++ service/lider_service.go
@@ -8,6 +8,7 @@ import (
 	"net"
 	"time"
 
+	"github.com/streadway/amqp"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
@@ -87,7 +88,47 @@ func CreateBots() {
 
 }
 
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func EnviarAPozoJugadorEliminado(id int32, etapa int32) {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	body := "Hello World!"
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+	log.Printf(" [x] Sent %s", body)
+}
+
 func main() {
+	EnviarAPozoJugadorEliminado(3, 2) //Ejemplo para enviar pozo a jugador eliminado
 
 	//Bienvenida e inicio del juego
 	fmt.Println("___Bienvenido al juego del Calamardo___")
